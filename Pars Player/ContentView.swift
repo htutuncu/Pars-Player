67b092b63a41d3b1b31cf6b9ab01fb5164cc100b
Hikmet Tutuncu <48223977+htutuncu@users.noreply.github.com>
2025-01-11 15:35:23 +0300

    Initial Commit

---
+++ Pars Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pars Player
+//
+//  Created by Hikmet Tütüncü on 11.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pars Player/Pars_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pars_PlayerApp.swift
+//  Pars Player
+//
+//  Created by Hikmet Tütüncü on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pars_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
